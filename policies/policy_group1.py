09185193cc7190f44cc26fcb096d1e8ab02ecb46
danielbarhanin <daniel.barhanin@healthy.io>
2023-02-13 15:30:45 +0200

    init class params

--- policies/policy_group1.py
+++ policies/policy_group1.py
@@ -4,6 +4,13 @@ import numpy as np
 
 class Group1(bp.Policy):
     
+    def __init__(self, policy_args, board_size, stateq, actq, modelq, logq, id, game_duration, score_scope):
+        super().__init__(policy_args, board_size, stateq, actq, modelq, logq, id, game_duration, score_scope)
+        self.table = {}
+        self.epsilon = 0.05
+        self.decay = 0.1
+        self.window_size = 3
+    
     def cast_string_args(self, policy_args):
         return policy_args
 
@@ -17,4 +24,6 @@ class Group1(bp.Policy):
         a =  np.random.choice(bp.Policy.ACTIONS)
         self.log(f"in round {round} usin action {a}" )
         return a
+    
+
 
